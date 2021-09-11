12a4279bf2358d0651477dcf4b1b2ec7b325c1ee
abdullah <bido6573@gmail.com>
2021-09-11 04:16:44 +0200

    2st commit to the app after finishing the design

--- app/src/main/java/com/abdullah/usicotask/ui/register/RegisterFragment.java
+++ app/src/main/java/com/abdullah/usicotask/ui/register/RegisterFragment.java
@@ -2,21 +2,47 @@ package com.abdullah.usicotask.ui.register;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.abdullah.usicotask.R;
+import com.abdullah.usicotask.databinding.FragmentRegisterBinding;
 
 public class RegisterFragment extends Fragment {
 
+    FragmentRegisterBinding binding;
+    NavController navController;
+    RegisterViewModel viewModel;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_register, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        viewModel = new ViewModelProvider(getActivity(), new ViewModelProvider.Factory() {
+            @NonNull
+            @Override
+            public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+                return null;
+            }
+        }).get(RegisterViewModel.class);
+
+        viewModel.register();
     }
 }
--- app/src/main/java/com/abdullah/usicotask/ui/register/RegisterViewModel.java
+++ app/src/main/java/com/abdullah/usicotask/ui/register/RegisterViewModel.java
@@ -1,4 +1,33 @@
 package com.abdullah.usicotask.ui.register;
 
-public class RegisterViewModel {
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.abdullah.usicotask.model.RetrofitClient;
+import com.abdullah.usicotask.model.requests.RegisterRequest;
+import com.abdullah.usicotask.model.responses.RegisterResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class RegisterViewModel extends ViewModel {
+
+    MutableLiveData<RegisterResponse> registerResponseMutableLiveData = new MutableLiveData<>();
+
+    public void register(){
+
+        RetrofitClient.getClient().register(new RegisterRequest())
+                .enqueue(new Callback<RegisterResponse>() {
+            @Override
+            public void onResponse(Call<RegisterResponse> call, Response<RegisterResponse> response) {
+                registerResponseMutableLiveData.postValue(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<RegisterResponse> call, Throwable t) {
+
+            }
+        });
+    }
 }
